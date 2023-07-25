563e909fd71792cae905d1cfaa9cc1bf35227519
sujin <csujin2040@gmail.com>
2023-07-25 20:39:07 +0900

    테스트

--- app/src/main/java/com/example/cambus/MainActivity.java
+++ app/src/main/java/com/example/cambus/MainActivity.java
@@ -2,17 +2,39 @@ package com.example.cambus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NavigationRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Fragment category;
+    Fragment school;
+    Fragment department;
+    Fragment user;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.school_home);
 
-    }
+        school = new Fragment();
+        department = new Fragment();
+        user = new Fragment();
 
-    public static class schoolhome {
+        getSupportFragmentManager().beginTransaction().replace(androidx.fragment.R.id.action_container, school).commit();
+
     }
 }
+
+
+
