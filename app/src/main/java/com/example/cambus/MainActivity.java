b2ffa6e19fc2eb9751feb385757ca9778ef3ed95
sujin <csujin2040@gmail.com>
2023-08-13 18:39:03 +0900

    화면 연결 80%

--- app/src/main/java/com/example/cambus/MainActivity.java
+++ app/src/main/java/com/example/cambus/MainActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private ImageButton button1;
+    private ImageButton button2; //학교공지로 가는 버튼
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,24 @@ public class MainActivity extends AppCompatActivity {
                     Intent intent = new Intent(MainActivity.this, fristbuyActivity.class);
                     startActivity(intent);
 
+                }
+            });
+
+            button1 = findViewById(R.id.ib_gobuy);
+            button1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, fristbuyActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+            button2 = findViewById(R.id.ib_maincardnews);
+            button2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, schoolnoticegoActivity.class);
+                    startActivity(intent);
                 }
             });
     }
